4b21ef6c9ddfbbd385c904cc3fd4f2110c7ac0de
Seko461 <sekopierre@gmail.com>
2024-03-20 00:13:36 +0100

    all milestones done

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -6,15 +6,26 @@ createApp({
         return {
             newTodo: '',
             todos: [],
+            hideCompleted: false,
+
+
         }
     },
     methods: {
         addTodo() {
-            this.todos.push({ id: id++, text: this.newTodo })
+            this.todos.push({ id: id++, text: this.newTodo, done: false })
             this.newTodo = ''
         },
         removeTodo(todo) {
             this.todos = this.todos.filter((t) => t !== todo)
+        },
+
+    },
+    computed: {
+        filteredTodos() {
+            return this.hideCompleted
+                ? this.todos.filter((t) => !t.done)
+                : this.todos
         }
     }
 }).mount('#app')
